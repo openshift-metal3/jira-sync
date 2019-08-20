db9351240c83ab2b035d600918a0288c030a9a30
Doug Hellmann <dhellmann@redhat.com>
2019-08-20 16:29:49 -0400

    add pagination handling for repo list
    The github API to list repositories does not return them all at
    once. Restructure the code to let us handle the pagination.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- main.go
+++ main.go
@@ -11,9 +11,142 @@ import (
 	"os"
 )
 
+type syncArgs struct {
+	githubClient      *github.Client
+	githubOrg         string
+	githubLabel       string
+	jiraURL           string
+	jiraClient        *jira.Client
+	jiraProject       string
+	jiraComponent     string
+	jiraIssueTypeName string
+}
+
+type callback func(syncArgs, *github.Repository) error
+
+func processAllRepositories(args syncArgs, callback callback) error {
+	ctx := context.Background()
+
+	opts := github.RepositoryListByOrgOptions{
+		Type: "all",
+	}
+
+	for {
+		page, response, err := args.githubClient.Repositories.ListByOrg(ctx, args.githubOrg, &opts)
+		if err != nil {
+			return fmt.Errorf("Failed to list %s repositories: %v", args.githubOrg, err)
+		}
+
+		for _, repo := range page {
+			if err = callback(args, repo); err != nil {
+				return err
+			}
+		}
+
+		if response.NextPage == 0 {
+			break
+		}
+		opts.Page = response.NextPage
+	}
+	return nil
+}
+
+func processOneRepository(args syncArgs, repo *github.Repository) error {
+
+	ghIssueQueryOpts := github.IssueListByRepoOptions{
+		State: "open",
+	}
+	if args.githubLabel != "" {
+		ghIssueQueryOpts.Labels = append(ghIssueQueryOpts.Labels, args.githubLabel)
+	}
+
+	issues, _, err := args.githubClient.Issues.ListByRepo(
+		context.Background(), args.githubOrg, *repo.Name, &ghIssueQueryOpts)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to list issues for %s: %s\n", repo, err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("\n%s\n", *repo.Name)
+
+	if len(issues) == 0 {
+		fmt.Printf("no issues\n")
+		return nil
+	}
+
+	for _, ghIssue := range issues {
+
+		if ghIssue.PullRequestLinks != nil {
+			// skip pull requests
+			continue
+		}
+
+		fmt.Printf("\n%d: [%6s] %s\n\t%s\n",
+			*ghIssue.Number, *ghIssue.State, *ghIssue.Title, *ghIssue.HTMLURL)
+
+		slug := fmt.Sprintf("github:%s:%s:%d", args.githubOrg, *repo.Name, *ghIssue.Number)
+
+		search := fmt.Sprintf("summary ~ \"%s\" and ( type = story or type = bug )", slug)
+
+		jiraIssues, _, err := args.jiraClient.Issue.Search(search, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to search for issue: %v", err)
+			continue
+		}
+
+		if len(jiraIssues) == 0 {
+			body := ""
+			if ghIssue.Body != nil {
+				body = *ghIssue.Body
+			}
+			issueParams := &jira.Issue{
+				Fields: &jira.IssueFields{
+					Project: jira.Project{
+						Key: args.jiraProject,
+					},
+					Components: []*jira.Component{
+						&jira.Component{
+							Name: args.jiraComponent,
+						},
+					},
+					Type: jira.IssueType{
+						Name: args.jiraIssueTypeName,
+					},
+					Summary: fmt.Sprintf("%s [%s]", *ghIssue.Title, slug),
+					Description: fmt.Sprintf("created automatically from %s\n\n%s",
+						*ghIssue.HTMLURL, body),
+				},
+			}
+			newJiraIssue, response, err := args.jiraClient.Issue.Create(issueParams)
+			if err != nil {
+				text, _ := ioutil.ReadAll(response.Body)
+				fmt.Fprintf(os.Stderr, "Failed to create issue: %s\n%s\n", err, text)
+				os.Exit(1)
+			}
+			fmt.Printf("CREATED %s %s/browse/%s\n",
+				newJiraIssue.Key,
+				args.jiraURL,
+				newJiraIssue.Key,
+			)
+		} else {
+			for _, jiraIssue := range jiraIssues {
+				fmt.Printf("EXISTING [%s] (%s) %s: %+v\n",
+					jiraIssue.Fields.Type.Name,
+					jiraIssue.Fields.Priority.Name,
+					jiraIssue.Key,
+					jiraIssue.Fields.Summary,
+				)
+			}
+		}
+	}
+
+	return nil
+}
+
 func main() {
 	token := flag.String("github-token", "", "the API token")
 	githubOrg := flag.String("github-org", "", "the organization to scan")
+	githubLabel := flag.String("github-label", "", "the issue label for filtering")
 	username := flag.String("jira-user", "", "the username")
 	password := flag.String("jira-password", "", "the password")
 	jiraURL := flag.String("jira-url", "", "the jira server URL")
@@ -57,25 +190,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Could not create client: %v", err)
 		os.Exit(1)
 	}
-
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: *token},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-
-	ghClient := github.NewClient(tc)
-
-	repos, _, err := ghClient.Repositories.ListByOrg(ctx, *githubOrg, nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to list %s repositories: %v", *githubOrg, err)
-		os.Exit(1)
-	}
-
-	ghIssueQueryOpts := github.IssueListByRepoOptions{
-		State: "open",
-	}
-
 	jiraCreateMeta, _, err := jiraClient.Issue.GetCreateMeta(*jiraProject)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to fetch metadata for %s: %s", *jiraProject, err)
@@ -84,80 +198,28 @@ func main() {
 	knideployProject := jiraCreateMeta.GetProjectWithKey(*jiraProject)
 	storyIssueType := knideployProject.GetIssueTypeWithName("story")
 
-	for _, repo := range repos {
-		issues, _, err := ghClient.Issues.ListByRepo(ctx, *githubOrg, *repo.Name, &ghIssueQueryOpts)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to list issues for %s: %s\n", repo, err)
-			os.Exit(1)
-		}
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
 
-		fmt.Printf("\n%s\n", *repo.Name)
+	githubClient := github.NewClient(tc)
 
-		if len(issues) == 0 {
-			fmt.Printf("no issues\n")
-			continue
-		}
+	args := syncArgs{
+		githubClient:      githubClient,
+		githubOrg:         *githubOrg,
+		githubLabel:       *githubLabel,
+		jiraURL:           *jiraURL,
+		jiraClient:        jiraClient,
+		jiraProject:       *jiraProject,
+		jiraComponent:     *jiraComponent,
+		jiraIssueTypeName: storyIssueType.Name,
+	}
 
-		for _, ghIssue := range issues {
-
-			if ghIssue.PullRequestLinks != nil {
-				// skip pull requests
-				continue
-			}
-
-			fmt.Printf("\n%d: [%6s] %s\n\t%s\n",
-				*ghIssue.Number, *ghIssue.State, *ghIssue.Title, *ghIssue.HTMLURL)
-
-			slug := fmt.Sprintf("github:%s:%s:%d", *githubOrg, *repo.Name, *ghIssue.Number)
-
-			search := fmt.Sprintf("summary ~ \"%s\" and ( type = story or type = bug )", slug)
-
-			jiraIssues, _, err := jiraClient.Issue.Search(search, nil)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to search for issue: %v", err)
-				continue
-			}
-
-			if len(jiraIssues) == 0 {
-				body := ""
-				if ghIssue.Body != nil {
-					body = *ghIssue.Body
-				}
-				issueParams := &jira.Issue{
-					Fields: &jira.IssueFields{
-						Project: jira.Project{
-							Key: *jiraProject,
-						},
-						Components: []*jira.Component{
-							&jira.Component{
-								Name: *jiraComponent,
-							},
-						},
-						Type: jira.IssueType{
-							Name: storyIssueType.Name,
-						},
-						Summary: fmt.Sprintf("%s [%s]", *ghIssue.Title, slug),
-						Description: fmt.Sprintf("created automatically from %s\n\n%s",
-							*ghIssue.HTMLURL, body),
-					},
-				}
-				newJiraIssue, response, err := jiraClient.Issue.Create(issueParams)
-				if err != nil {
-					text, _ := ioutil.ReadAll(response.Body)
-					fmt.Fprintf(os.Stderr, "Failed to create issue: %s\n%s\n", err, text)
-					os.Exit(1)
-				}
-				fmt.Printf("CREATED %s %s/browse/%s\n", newJiraIssue.Key, *jiraURL, newJiraIssue.Key)
-			} else {
-				for _, jiraIssue := range jiraIssues {
-					fmt.Printf("EXISTING [%s] (%s) %s: %+v\n",
-						jiraIssue.Fields.Type.Name,
-						jiraIssue.Fields.Priority.Name,
-						jiraIssue.Key,
-						jiraIssue.Fields.Summary,
-					)
-				}
-			}
-		}
+	err = processAllRepositories(args, processOneRepository)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v", err)
+		os.Exit(1)
 	}
 }
