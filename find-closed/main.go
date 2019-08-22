fb5ee3da66afb133b9ee9dd7b98236201b242c00
Doug Hellmann <dhellmann@redhat.com>
2019-08-22 16:29:30 -0400

    leave a comment on the jira ticket when the upstream ticket is closed
    Comment one time when we discover that the upstream ticket has closed
    but the jira ticket is still open.
    
    Signed-off-by: Doug Hellmann <dhellmann@redhat.com>

--- find-closed/main.go
+++ find-closed/main.go
@@ -1,25 +1,30 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/andygrunwald/go-jira"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
 
+const closedCommentMessage = "The upstream ticket has been closed."
+
 type syncArgs struct {
-	bugzillaURL string
-	// bugzillaProduct   string
-	// bugzillaToken     string
-	jiraURL     string
-	jiraClient  *jira.Client
-	jiraProject string
+	bugzillaURL  string
+	githubClient *github.Client
+	jiraURL      string
+	jiraClient   *jira.Client
+	jiraProject  string
 }
 
 type bug struct {
@@ -52,8 +57,11 @@ func reportClosedIssues(args syncArgs) error {
 	bzClient := http.Client{
 		Timeout: time.Second * 2, // Maximum of 2 secs
 	}
+	ctx := context.Background()
 
 	for _, jiraIssue := range jiraIssues {
+
+		isClosed := false
 
 		fmt.Printf("%s", jiraIssue.Key)
 		match := linkSearch.FindStringSubmatch(jiraIssue.Fields.Description)
@@ -65,8 +73,21 @@ func reportClosedIssues(args syncArgs) error {
 
 		case "github":
 			fields := strings.Split(match[2], ":")
-			fmt.Printf("\torg = %q repo = %q issue = %q\n",
+			fmt.Printf("\torg = %q repo = %q issue = %q",
 				fields[0], fields[1], fields[2])
+
+			issueNum, err := strconv.Atoi(fields[2])
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+				continue
+			}
+			ghIssue, _, err := args.githubClient.Issues.Get(ctx, fields[0], fields[1], issueNum)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+				continue
+			}
+
+			isClosed = (*ghIssue.State == "closed")
 
 		case "bugzilla":
 			fmt.Printf("\tbz = %s", match[2])
@@ -98,17 +119,54 @@ func reportClosedIssues(args syncArgs) error {
 				continue
 			}
 
-			status := bz.Bugs[0].Status
-
-			if status == "CLOSED" {
-				fmt.Printf(" status = %s", status)
-			}
-
-			fmt.Printf("\n")
+			isClosed = bz.Bugs[0].Status == "CLOSED"
 
 		default:
 			fmt.Fprintf(os.Stderr, "ERROR:Could not parse %q\n", match[0])
 		}
+
+		if !isClosed {
+			fmt.Printf("\n")
+			continue
+		}
+
+		fmt.Printf(" CLOSED")
+
+		needToAdd := true
+
+		// The search results do not include comments, so we have to
+		// fetch tickets when we need the comments.
+		commentedIssue, _, err := args.jiraClient.Issue.Get(jiraIssue.Key, nil)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR fetching issue %s: %s\n", jiraIssue.Key, err)
+			continue
+		}
+
+		if commentedIssue.Fields.Comments != nil {
+			for _, comment := range commentedIssue.Fields.Comments.Comments {
+				if comment.Body == closedCommentMessage {
+					needToAdd = false
+					break
+				}
+			}
+		} else {
+			fmt.Printf(" nil comments")
+		}
+
+		if needToAdd {
+			newComment := jira.Comment{
+				Body: closedCommentMessage,
+			}
+			_, _, err := args.jiraClient.Issue.AddComment(jiraIssue.ID, &newComment)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "ERROR adding comment: %s\n", err)
+				continue
+			}
+			fmt.Printf(" UPDATED")
+		}
+
+		fmt.Printf("\n")
+
 	}
 
 	return nil
@@ -120,6 +178,7 @@ func main() {
 	password := flag.String("jira-password", "", "the password")
 	jiraURL := flag.String("jira-url", "", "the jira server URL")
 	jiraProject := flag.String("jira-project", "", "the jira project")
+	token := flag.String("github-token", "", "the API token")
 
 	flag.Parse()
 
@@ -143,6 +202,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	if *token == "" {
+		fmt.Fprintf(os.Stderr, "Please provide an API token (-github-token)")
+		os.Exit(1)
+	}
+
 	tp := jira.BasicAuthTransport{
 		Username: *username,
 		Password: *password,
@@ -154,13 +218,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: *token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	githubClient := github.NewClient(tc)
+
 	args := syncArgs{
-		bugzillaURL: *bugzillaURL,
-		// bugzillaProduct:   *bugzillaProduct,
-		// bugzillaToken:     *token,
-		jiraURL:     *jiraURL,
-		jiraClient:  jiraClient,
-		jiraProject: *jiraProject,
+		bugzillaURL:  *bugzillaURL,
+		githubClient: githubClient,
+		jiraURL:      *jiraURL,
+		jiraClient:   jiraClient,
+		jiraProject:  *jiraProject,
 	}
 
 	err = reportClosedIssues(args)
